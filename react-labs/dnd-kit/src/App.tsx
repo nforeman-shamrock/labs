4c24e0cab9fd6385fbf6ebac0a0d792640a5b919
Nathan Foreman <nforeman@ryanrts.com>
2022-10-03 14:20:06 -0500

    Add React-lab

---
+++ react-labs/dnd-kit/src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { DndContext, DragEndEvent } from '@dnd-kit/core';
+
+import { Droppable } from './Droppable';
+import { Draggable } from './Draggable';
+
+export default function App() {
+  const [isDropped, setIsDropped] = useState(false);
+  const draggableMarkup = <Draggable>Drag me</Draggable>;
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    if (event.over && event.over.id === 'droppable') {
+      setIsDropped(true);
+    }
+  };
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      {!isDropped ? draggableMarkup : null}
+      <Droppable>{isDropped ? draggableMarkup : 'Drop here'}</Droppable>
+    </DndContext>
+  );
+}
